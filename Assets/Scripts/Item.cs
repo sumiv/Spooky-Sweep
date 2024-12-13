90972c43e45927eff379b83492596eef8d93277d
sumiverma <sumistargazer@gmail.com>
2024-12-13 09:00:54 -0500

    score saved

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    //public int points = 1;
+    public int points = 1;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //FindObjectOfType<ScoreManager>().AddScore(points);
+            FindObjectOfType<ScoreManager>().AddScore(points);
             GameManager.instance.CollectItem();
             Destroy(gameObject);
         }
